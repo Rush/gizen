b6d9724077b694a99f3b67c2abbf618fb332da21
Mateus Freira <mateus.freira@gmail.com>
2017-10-10 20:03:10 -0300

    check current folder before checking the home folder

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Promise = require('bluebird');
 const GitHubApi = require("github");
 
 const readline = require('readline');
-const {readFileSync} = require('fs');
+const {readFileSync, existsSync} = require('fs');
 const {join: pathJoin } = require('path');
 
 let rl;
@@ -31,8 +31,16 @@ var github = new GitHubApi({
 const ZenHub = require('zenhub-api');
   
 let config;
+const foldersToSearch = ['./', process.env.HOME];
+
 try {
-  config = JSON.parse(readFileSync(pathJoin(process.env.HOME, '.gizen')));
+  foldersToSearch.every((folder) => {
+    const fileName = pathJoin(folder, '.gizen');
+    if (existsSync(fileName)) {
+      config = JSON.parse(readFileSync(fileName));
+    }
+    return !config;
+  });
 } catch(err) {  
   console.error(err.message);
   console.error('');
